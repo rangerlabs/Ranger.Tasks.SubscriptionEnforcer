77f4465994c059a4129a580ed428d7f7c00153f5
Nick Cromwell <cromwellnick@gmail.com>
2020-09-04 12:00:11 -0400

    Add stackexchange feed

--- scripts/create-nuget-config.sh
+++ scripts/create-nuget-config.sh
@@ -3,6 +3,7 @@ echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
     <packageSources>
         <add key=\"NuGet\" value=\"https://api.nuget.org/v3/index.json\" />
         <add key=\"MyGet\" value=\"https://www.myget.org/F/ranger-labs/auth/$1/api/v3/index.json\" />
+        <add key=\"StackExchange\" value=\"https://www.myget.org/F/stackoverflow/api/v3/index.json\" />
     </packageSources>
     <activePackageSource>
         <add key=\"All\" value=\"(Aggregate source)\" />
